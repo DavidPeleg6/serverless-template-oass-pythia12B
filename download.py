597ec9b5a84d167a30687ad85e7a8e1b052fe750
davidp <davidp@aui.io>
2023-04-25 08:54:51 +0300

    test

--- download.py
+++ download.py
@@ -9,6 +9,7 @@ def download_model():
     # do a dry run of loading the huggingface model, which will download weights
     tokenizer = AutoTokenizer.from_pretrained("stabilityai/stablelm-tuned-alpha-7b")
     model = AutoModelForCausalLM.from_pretrained("stabilityai/stablelm-tuned-alpha-7b")
+    print('temp test')
 
 if __name__ == "__main__":
     download_model()
